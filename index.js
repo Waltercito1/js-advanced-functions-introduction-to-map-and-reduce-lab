6fbe7af714be130704d9569e01717389225a6a0f
Walter Aab <aabwalter@yahoo.com>
2021-05-20 18:56:42 -0500

    commit tree 1618042a5a1134923e166c0b8443e412d85d4c2c

--- index.js
+++ index.js
@@ -1,1 +1,57 @@
 // Your code here
+function mapToNegativize(arr) {
+    let r = []
+    for (let i = 0; i < arr.length; i++) {
+        r.push(-1 * arr[i])
+    }
+    return r
+}
+
+function mapToNoChange(arr) {
+    let r = []
+    for (let i = 0; i < arr.length; i++) {
+        r.push(arr[i])
+    }
+    return r
+}
+
+function mapToDouble(arr) {
+    let r = []
+    for (let i = 0; i < arr.length; i++) {
+        r.push(2 * arr[i])
+    }
+    return r
+}
+
+function mapToSquare(arr) {
+    let r = []
+    for (let i = 0; i < arr.length; i++) {
+        r.push(arr[i] * arr[i])
+    }
+    return r
+}
+
+function reduceToTotal(arr, start=0) {
+    let total = start
+    for (let i = 0; i < arr.length; i++) {
+        total = total + arr[i]
+    }
+    return total
+}
+
+function reduceToAllTrue(arr) {
+    for (let i = 0; i < arr.length; i++) {
+        if (!arr[i]) return false
+    }
+    return true
+}
+
+function reduceToAnyTrue(arr) {
+    for (let i = 0; i < arr.length; i++) {
+        if (arr[i]) return true
+    }
+    return false
+}
+
+
+  
